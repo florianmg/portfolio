7dd4f0d7ca003cd40fc66a0879ef6ea8cbe703fd
admin <admin@iMac3.local>
2018-09-05 16:29:53 +0200

    Scroll smooth

--- js/anime.js
+++ js/anime.js
@@ -1,36 +1,49 @@
-var svgAttributes = anime({
-    targets: '#svgAttributes polygon',
-    points: '64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96',
-    duration: 2000,
-    easing: 'easeInOutExpo'
-});
 
-var nameAppear = anime({
-    targets:'#txtSvg',
-    opacity: 1,
-    delay: 2000,
-    duration: 3000
-});
+$(document).ready(function() {
 
-var jobAppear = anime({
-    targets:'#job',
-    opacity: 1,
-    delay: 2000,
-    duration: 3000
-});
+    var svgAttributes = anime({
+        targets: '#svgAttributes polygon',
+        points: '64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96',
+        duration: 2000,
+        easing: 'easeInOutExpo'
+    });
 
-var svgArrow = anime({
-    targets: '#arrow_bot',
-    delay: 2300,
-    opacity: 1,
-    translateY: 50,
-    easing: 'easeInOutQuart',
-    scale: {
-        value: 2,
-        duration: 500,
-        delay: 3000,
-        loop: true,
-        easing: 'easeInOutQuart'
-    }
+    var nameAppear = anime({
+        targets:'#txtSvg',
+        opacity: 1,
+        delay: 2000,
+        duration: 3000
+    });
 
+    var jobAppear = anime({
+        targets:'#job',
+        opacity: 1,
+        delay: 2000,
+        duration: 3000
+    });
+
+    var svgArrow = anime({
+        targets: '#arrow_bot',
+        delay: 2300,
+        opacity: 1,
+        translateY: 50,
+        easing: 'easeInOutQuart',
+        scale: {
+            value: 2,
+            duration: 500,
+            delay: 3000,
+            loop: true,
+            easing: 'easeInOutQuart'
+        }
+
+    });
+
+
+
+    $('.scroll').on('click', function() { // on Click element class scroll
+        var page = $(this).attr('href'); // Stock dans la var valleur de l'attribut
+        var speed = 750; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+        return false;
+    });
 });
