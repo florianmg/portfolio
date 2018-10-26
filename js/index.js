d864c6ec2a6cd2005a4be6c5b51c521a586ef866
admin <admin@iMac3.local>
2018-10-26 16:45:25 +0200

    fixes style

--- js/index.js
+++ js/index.js
@@ -9,4 +9,59 @@ $(document).ready(function() {
     });
 
 
+   
+
 });
+
+function getAjax() {
+
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.open("GET", "projects.json", false);
+    //debugger;
+    xhttp.send(null);
+   
+    let results = JSON.parse(xhttp.response)
+    
+    
+
+    for (let i = 0; i < results.length; i++) {
+        let name = results[i].name;
+        let languages = results[i].languages;
+        let url = results[i].url;
+        let description = results[i].description;
+
+        let line = document.createElement("DIV");
+        let left = document.createElement("DIV");
+        let right = document.createElement("DIV");
+        let title = document.createElement("P");
+        let lang = document.createElement("P");
+        let img = document.createElement("IMG");
+        let descr = document.createElement("P");
+
+        title.innerHTML = name;
+        lang.innerHTML = languages;
+        descr.innerHTML = description;
+        img.src = url;
+
+        console.log(url);
+        line.classList.add('line');
+        left.classList.add('left');
+        right.classList.add('right');
+     
+
+        if(i%2) {
+            line.classList.add('pair');
+        }
+        left.appendChild(img);
+        line.appendChild(left);
+        right.appendChild(title);
+        right.appendChild(descr);
+        right.appendChild(lang);
+        line.appendChild(right);
+        document.querySelector('.projects').appendChild(line);
+
+    }
+}
+
+//getAjax();
